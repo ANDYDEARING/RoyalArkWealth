22b280955535f8e8344936b7d5d8b8d85edd9ded
Andy Dearing <andy.dearing@gmail.com>
2020-05-07 16:42:35 -0400

    added js questions

--- quiz.js
+++ quiz.js
@@ -1,5 +1,25 @@
-var tag = document.createElement("p");
-var text = document.createTextNode("Inernal JS Injection Works");
-tag.appendChild(text);
-var element = document.getElementsByTagName('quiz-area')[0];
-element.appendChild(tag);
+//Heading
+var headerTag = document.createElement("h1");
+var headerText = document.createTextNode("Quiz");
+headerTag.appendChild(headerText);
+var quizArea = document.getElementsByTagName('quiz-area')[0];
+quizArea.appendChild(headerTag);
+
+//Question Area
+var questionTag = document.createElement("p");
+questionTag.innerText = "What is your time worth?";
+quizArea.appendChild(questionTag);
+
+//Answer Area
+var answerAreaTag = document.createElement("div");
+answerAreaTag.innerHTML= '<span>$ </span><input id="answer" type="number"></input><span> / Hour</span>';
+quizArea.appendChild(answerAreaTag);
+
+//Next Button
+var buttonAreaTag = document.createElement("div");
+buttonAreaTag.innerHTML = '<br><button onClick="next()">Next</button>';
+quizArea.appendChild(buttonAreaTag);
+
+function next(){
+    console.log(document.getElementById("answer").value);
+}
